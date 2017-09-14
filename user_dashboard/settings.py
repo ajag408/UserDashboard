a3ecda6f68914dd4d9e579b4e9850291bdb379d9
Akash Jag <akashjagannathan408@gmail.com>
2017-09-14 16:21:35 -0700

    xframe

--- user_dashboard/settings.py
+++ user_dashboard/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
+
+XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
